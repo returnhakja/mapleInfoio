f15fd2154f07c1f4016cdf8544b8a9969bd1cfae
도헌's <62533556+returnhakja@users.noreply.github.com>
2024-04-20 23:31:18 +0900

    test commit

--- my-app/src/components/infoPage/MainInfoPage.tsx
+++ my-app/src/components/infoPage/MainInfoPage.tsx
@@ -1,6 +1,7 @@
 import style from "./page.module.css";
 import Content from "../container/Content";
 import Gnb from "../header/Gnb";
+import test from "../../assets/logo.png";
 
 export const MainInfoPage = () => {
   const contents = [
@@ -15,8 +16,9 @@ export const MainInfoPage = () => {
         <Gnb />
       </header>
       <div className={style.container}>
+        <img src={test} alt="test" />
         {contents.map((item) => (
-          <Content heading={item.heading} />
+          <Content heading={item.heading} key={item.heading} />
         ))}
       </div>
     </>
