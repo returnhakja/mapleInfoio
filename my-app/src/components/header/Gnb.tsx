7c6ba92c3fac2ed4b7f23080c73e15db3141e4e0
bgyuee <127265942+bgyuee@users.noreply.github.com>
2024-04-24 07:31:47 +0900

    header 레이아웃, css 수정

--- my-app/src/components/header/Gnb.tsx
+++ my-app/src/components/header/Gnb.tsx
@@ -1,16 +1,18 @@
 /** @jsxImportSource @emotion/react */
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Logo from "../../assets/logo.png";
 import { useInfo } from "../../hooks/useInfo.hooks";
 import { Button } from "../common/Button";
 import { TextInput } from "../common/TextInput";
 import style from "../infoPage/page.module.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setOcid } from "../../states/client/userOcid.ts/ocid";
 import * as Styles from "./Header.styles";
+import { RootState } from "../../states/client";
 export const Gnb = () => {
   const [nickName, setNickName] = useState("");
+  const ocId = useSelector((state: RootState) => state.ocId);
   const app = useInfo({ nickName });
   const dispatch = useDispatch();
   const reFetchData = () => {
@@ -19,20 +21,19 @@ export const Gnb = () => {
   };
   return (
     <div css={Styles.HeaderCon}>
-      <img
-        css={Styles.Logo}
-        src={Logo}
-        alt="Logo"
-        onClick={() => (window.location.href = "/")}
-      />
-      <div className={style.inputCon}>
-        <TextInput
-          className={style.inPut}
-          placeholder="닉네임을 입력하세요."
-          onChange={(e: any) => setNickName(e.target.value)}
-        />
-        <Button text="조회" className={style.button} onClick={reFetchData} />
-      </div>
+      <figure css={Styles.Logo} onClick={() => (window.location.href = "/")}>
+        <img src={Logo} alt="Logo" />
+      </figure>
+      {ocId ? (
+        <div css={Styles.inputCon}>
+          <TextInput
+            className={style.inPut}
+            placeholder="닉네임을 입력하세요."
+            onChange={(e: any) => setNickName(e.target.value)}
+          />
+          <Button text="조회" className={style.button} onClick={reFetchData} />
+        </div>
+      ) : null}
     </div>
   );
 };
--- my-app/src/components/header/Header.styles.ts
+++ my-app/src/components/header/Header.styles.ts
@@ -13,4 +13,15 @@ export const HeaderCon = css`
 export const Logo = css`
   width: 100px;
   height: 100px;
+  cursor: pointer;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const inputCon = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
